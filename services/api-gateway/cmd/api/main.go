48645873bbd58696dfc9f0948ca55b0b2d9227af
Mateus Lima de Freitas <dev.org.ml@gmail.com>
2026-01-02 15:47:12 -0300

    feat(api): add api-gateway service skeleton with health check

---
+++ services/api-gateway/cmd/api/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	})
+
+	log.Println("api-gateway running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
