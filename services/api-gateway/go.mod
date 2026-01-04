81bfddbdd6688d1e5d4d317da04a13364d9bd271
Mateus Lima de Freitas <dev.org.ml@gmail.com>
2026-01-04 09:56:43 -0300

    feat(api-gateway): improve create deposit resource and add dependencies for the module

--- services/api-gateway/go.mod
+++ services/api-gateway/go.mod
@@ -1,3 +1,5 @@
 module github.com/matlimaaa/fx-ledger-platform/services/api-gateway
 
 go 1.22.2
+
+require github.com/google/uuid v1.6.0
--- services/api-gateway/internal/usecase/create_deposit.go
+++ services/api-gateway/internal/usecase/create_deposit.go
@@ -1,6 +1,19 @@
 package usecase
 
-import "github.com/matlimaaa/fx-ledger-platform/services/api-gateway/internal/domain"
+import (
+	"github.com/google/uuid"
+	"github.com/matlimaaa/fx-ledger-platform/services/api-gateway/internal/domain"
+)
+
+type CreateDepositInput struct {
+	WalletID string
+	Amount   int64
+	Currency string
+}
+
+type CreateDepositOutput struct {
+	DepositID string
+}
 
 type DepositRepository interface {
 	Save(deposit domain.Deposit) error
@@ -14,6 +27,27 @@ func NewCreateDepositUseCase(repo DepositRepository) *CreateDepositUseCase {
 	return &CreateDepositUseCase{repo: repo}
 }
 
-func (uc *CreateDepositUseCase) Execute(deposit domain.Deposit) error {
-	return uc.repo.Save(deposit)
+func (uc *CreateDepositUseCase) Execute(input CreateDepositInput) (CreateDepositOutput, error) {
+	money, error := domain.NewMoney(input.Amount, input.Currency)
+	if error != nil {
+		return CreateDepositOutput{}, error
+	}
+
+	deposit, error := domain.NewDeposit(
+		uuid.NewString(),
+		input.WalletID,
+		money,
+	)
+
+	if error != nil {
+		return CreateDepositOutput{}, error
+	}
+
+	if error := uc.repo.Save(deposit); error != nil {
+		return CreateDepositOutput{}, error
+	}
+
+	return CreateDepositOutput{
+		DepositID: deposit.ID,
+	}, nil
 }
